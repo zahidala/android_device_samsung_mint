eaeb77725cfc87a42211884a9b3dce5bf61af24c
zahid <zahid@zahid-VirtualBox.(none)>
2013-12-31 00:42:19 +0300

    added more missing files(still WIP)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,30 +1,30 @@
-USE_CAMERA_STUB := true
-
 # inherit from the proprietary version
--include vendor/samsung/sp8810/BoardConfigVendor.mk
+-include vendor/samsung/mint/BoardConfigVendor.mk
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := sc8810
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_BOOTLOADER_BOARD_NAME := sp8810
+TARGET_BOOTLOADER_BOARD_NAME := mint
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a
 TARGET_ARCH_VARIANT_CPU := cortex-a5
 TARGET_CPU_VARIANT := cortex-a5
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+# kernel
 BOARD_KERNEL_CMDLINE := console=ttyS1,115200n8
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
+TARGET_KERNEL_SOURCE := kernel/samsung/mint
+TARGET_KERNEL_CONFIG :=cyanogenmod_mint_defconfig
+BOARD_USES_UNCOMPRESSED_BOOT := true
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 939524096
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2172649472
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_PREBUILT_KERNEL := device/samsung/sp8810/kernel
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 
@@ -38,15 +38,67 @@ BOARD_DATA_DEVICE := /dev/block/mmcblk0p25
 BOARD_DATA_FILESYSTEM := ext4
 TARGET_USERIMAGES_USE_EXT4 := true
 
+# boot
+TARGET_PROVIDES_INIT_TARGET_RC := true
+
+# recovery
 BOARD_LDPI_RECOVERY := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/sp8810/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/sp8810/recovery/graphics.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/mint/recovery/recovery_keys.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/mint/recovery/graphics.c
 BOARD_HAS_NO_MISC_PARTITION := true
-TARGET_RECOVERY_FSTAB := device/samsung/sp8810/recovery/recovery.fstab
-TARGET_RECOVERY_INITRC := device/samsung/sp8810/recovery/recovery.rc
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/dwc_otg.0/gadget/lun0/file"
+TARGET_RECOVERY_FSTAB := device/samsung/mint/recovery/recovery.fstab
+TARGET_RECOVERY_INITRC := device/samsung/mint/recovery/recovery.rc
 BOARD_SUPPRESS_EMMC_WIPE := true
 
+#ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
+#ADDITIONAL_DEFAULT_PROPERTIES += ro.debuggable=1
+#ADDITIONAL_DEFAULT_PROPERTIES += service.adb.root=1
+#ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=adb
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0 \
+    ro.debuggable=1 \
+    service.adb.root=1
+
+# UMS
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/dwc_otg.0/gadget/lun0/file"
+
+# graphic
+BOARD_EGL_CFG := device/samsung/mint/egl/egl.cfg
 USE_OPENGL_RENDERER := true
+
+# Camera
+USE_CAMERA_STUB := true
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_bcmdhd
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WLAN_DEVICE_REV       := bcm4330
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_AP_ARG   := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                   := 802_11_ABG
+
+# bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/mint/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/mint/bluetooth/libbt_vndcfg.txt
+
+# ril
+TARGET_PROVIDES_LIBRIL := true
+BOARD_USES_LEGACY_RIL := true
+BOARD_FORCE_RILD_AS_ROOT := true
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+BOARD_RIL_CLASS := ../../../device/samsung/msm7x27-common/ril/
+
+WITH_DEXPREOPT := true
+
--- device_mint.mk
+++ device_mint.mk
@@ -8,34 +8,48 @@ $(call inherit-product-if-exists, vendor/samsung/mint/mint-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/samsung/mint/overlay
 
 LOCAL_PATH := device/samsung/mint
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+
+#
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+#else
+#	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 #PRODUCT_COPY_FILES += \
 #    $(LOCAL_KERNEL):kernel
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/root/fstab.sp8810:root/fstab.sp8810 \
-    $(LOCAL_PATH)/prebuilt/root/init.bt.rc:root/init.bt.rc \
-    $(LOCAL_PATH)/prebuilt/root/init.sp8810.rc:root/init.sp8810.rc \
-    $(LOCAL_PATH)/prebuilt/root/init.sp8810.usb.rc:root/init.sp8810.usb.rc \
-    $(LOCAL_PATH)/prebuilt/root/lpm.rc:root/lpm.rc \
-    $(LOCAL_PATH)/prebuilt/root/ueventd.sp8810.rc:root/ueventd.sp8810.rc
+$(shell mkdir -p $(LOCAL_PATH)/../../../out/target/product/mint/recovery/root/system/bin)
+$(shell ln -sf -t $(LOCAL_PATH)/../../../out/target/product/mint/recovery/root/system/bin ../../sbin/sh)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/recovery.rc:root/recovery.rc \
+    $(LOCAL_PATH)/boot/init.goldfish.rc:root/init.goldfish.rc \
+    $(LOCAL_PATH)/boot/init.rc:root/init.rc \
+    $(LOCAL_PATH)/boot/init.sp8810.rc:root/init.sp8810.rc \
+    $(LOCAL_PATH)/boot/init.sp8810.usb.rc:root/init.sp8810.usb.rc \
+    $(LOCAL_PATH)/boot/init.usb.rc:root/init.usb.rc \
+    $(LOCAL_PATH)/boot/init.trace.rc:root/init.trace.rc
+
+$(shell mkdir -p $(LOCAL_PATH)/../../../out/target/product/mint/root/bin)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/boot/charge:root/bin/charge \
+    $(LOCAL_PATH)/boot/vcharged:root/bin/vcharged
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/prebuilt/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/prebuilt/system/etc/audio_profiles.xml:system/etc/audio_profiles.xml \
-    $(LOCAL_PATH)/prebuilt/system/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    $(LOCAL_PATH)/prebuilt/system/lib/lights.sp8810.so:system/lib/lights.sp8810.so
+    $(LOCAL_PATH)/boot/fstab.sp8810:root/fstab.sp8810
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/boot/ueventd.rc:root/ueventd.rc \
+    $(LOCAL_PATH)/boot/ueventd.goldfish.rc:root/ueventd.goldfish.rc \
+    $(LOCAL_PATH)/boot/ueventd.sp8810.rc:root/ueventd.sp8810.rc
 
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_mint
 PRODUCT_DEVICE := mint
+
+TARGET_BUILD_VARIANT := userdebug
+#PRODUCT_LOCALES := it_IT en_US
+PRODUCT_AAPT_CONFIG := normal ldpi mdpi nodpi
+PRODUCT_AAPT_PREF_CONFIG := ldpi
+
